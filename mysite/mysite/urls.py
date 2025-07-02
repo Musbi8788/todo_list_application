1f27b8dccc63921ae755bf4779ac8f4d00afcb98
Musbi <musbi700@gmail.com>
2025-07-02 21:38:41 +0100

    updated

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# importing register folder
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', v.register, name="register"),
     path('', include("main.urls")),
 
 ]
